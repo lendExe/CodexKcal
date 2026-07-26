1ff409fe671f570bd3f97fa26b57c8d2d7284e8b
lendExe <141554845+lendExe@users.noreply.github.com>
2026-07-27 00:38:53 +0300

    remove deprecated resource pack format range fields for 26.2

--- patch_hmi_26_2_resourcepack.py
+++ patch_hmi_26_2_resourcepack.py
@@ -5,7 +5,6 @@ path = Path('hmi-source/mod/src/main/resources/resourcepacks/pack_test/pack.mcme
 data = json.loads(path.read_text(encoding='utf-8'))
 pack = data.setdefault('pack', {})
 pack['pack_format'] = 88
-pack['supported_formats'] = [88, 88]
-pack['min_format'] = 88
-pack['max_format'] = 88
+for obsolete in ('supported_formats', 'min_format', 'max_format'):
+    pack.pop(obsolete, None)
 path.write_text(json.dumps(data, indent=2) + '\n', encoding='utf-8')
